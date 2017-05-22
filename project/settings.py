fec977220e1f5e4c8a03b2b11bbfb3bed5315841
Alex Clark <aclark@aclark.net>
2017-05-21 20:17:03 -0400

    Update

--- project/settings.py
+++ project/settings.py
@@ -47,7 +47,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [{
