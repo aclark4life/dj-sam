f0d5920c671a8203f934337f157b69fb86542b28
Alex Clark <aclark@aclark.net>
2017-05-21 22:22:55 -0400

    Update

--- project/app/views.py
+++ project/app/views.py
@@ -32,7 +32,7 @@ SAML2_RESPONSE = """
   <samlp:Status>
     <samlp:StatusCode Value="urn:oasis:names:tc:SAML:2.0:status:Success"/>
   </samlp:Status>
-  <saml:Assertion xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" ID="pfx6a4934ca-8e10-1df0-7a43-527877a49cfa" Version="2.0" IssueInstant="%s">
+  <saml:Assertion xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" ID="%s" Version="2.0" IssueInstant="%s">
     <saml:Issuer>http://idp.example.com/metadata.php</saml:Issuer><ds:Signature xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
   <ds:SignedInfo><ds:CanonicalizationMethod Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/>
     <ds:SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#rsa-sha1"/>
@@ -85,7 +85,7 @@ def home(request):
 
     # saml2_response = SAML2_RESPONSE % (response_id, issue_instant,
     #                                    assertion_id, issue_instant, issue_instant)
-    saml2_response = SAML2_RESPONSE % (response_id, issue_instant, destination, issue_instant)
+    saml2_response = SAML2_RESPONSE % (response_id, issue_instant, destination, assertion_id, issue_instant)
 
     root = etree.fromstring(saml2_response)
     saml2_response = etree.tostring(root, pretty_print=True)
